f5143029b4eadf3bca707518f8ec38d1b99dc8e7
Ivan Kolchev <vanya@web-imago.ru>
2021-11-22 22:45:48 +0300

    Первый коммит

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:notion_client/presentation/app.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
---
+++ lib/presentation/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Notion Client'),
+        ),
+      ),
+    );
+  }
+}
