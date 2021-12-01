7c9fe01cc371abf0558d518a955962686838b1f2
Kokindos <kokindoss@gmail.com>
2021-12-01 14:43:02 +0300

    create splash_page

--- lib/presentation/pages/splash_page.dart
+++ lib/presentation/pages/splash_page.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:notion_client/presentation/assets_paths/resources.dart';
 
-class SplashPage extends StatefulWidget {
+class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        //TODO: add image
-        child: Text('Splash'),
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 100),
+        constraints: const BoxConstraints(
+          minWidth: 212,
+        ),
+        child: SvgPicture.asset(
+          SvgPath.logo,
+        ),
       ),
     );
   }
