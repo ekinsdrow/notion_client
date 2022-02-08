5dd3f5bd7be2511bbf6001ac6e77426fdd5083a3
Ivan Kolchev <vanya@web-imago.ru>
2022-02-08 21:23:36 +0300

    Create custom scrollbar

--- lib/presentation/components/horizontal_pages_listview.dart
+++ lib/presentation/components/horizontal_pages_listview.dart
@@ -11,12 +11,20 @@ class HorizontalPagesListView extends StatefulWidget {
 }
 
 class _HorizontalPagesListViewState extends State<HorizontalPagesListView> {
-  final _scrollController = ScrollController();
+  var _lineLeft = 0.0;
 
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
+  bool _scrollNotification(ScrollNotification notification) {
+    setState(() {
+      final max = notification.metrics.maxScrollExtent;
+      final now = notification.metrics.pixels;
+      _lineLeft = (now / max * 100) * 1.5;
+
+      print('LEFT $_lineLeft');
+      print('MAX $max');
+      print('NOW $now');
+    });
+
+    return true;
   }
 
   @override
@@ -25,67 +33,98 @@ class _HorizontalPagesListViewState extends State<HorizontalPagesListView> {
       children: [
         SizedBox(
           height: 110,
-          child: ListView.separated(
-            controller: _scrollController,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) => UnconstrainedBox(
-              child: Container(
-                width: 100,
-                height: 100,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 5,
-                  vertical: 10,
-                ),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFF5F4F4),
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 4,
-                      color: Colors.black.withOpacity(.07),
-                      offset: const Offset(
-                        5,
-                        4,
+          child: NotificationListener<ScrollNotification>(
+            onNotification: _scrollNotification,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => UnconstrainedBox(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 5,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF5F4F4),
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 4,
+                        color: Colors.black.withOpacity(.07),
+                        offset: const Offset(
+                          5,
+                          4,
+                        ),
+                        spreadRadius: 0,
                       ),
-                      spreadRadius: 0,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Text(
-                      '🥺',
-                      style: TextStyle(
-                        fontSize: 25,
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Text(
+                        '🥺',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 7,
-                    ),
-                    Text(
-                      'Page',
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 11,
+                      SizedBox(
+                        height: 7,
                       ),
-                    ),
-                  ],
+                      Text(
+                        'Page',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 11,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
+              separatorBuilder: (context, index) => const SizedBox(
+                width: paddingsMedium,
+              ),
+              itemCount: 5,
             ),
-            separatorBuilder: (context, index) => const SizedBox(
-              width: paddingsMedium,
-            ),
-            itemCount: 5,
           ),
         ),
         const SizedBox(
           height: paddingsMedium,
         ),
-
-        //TODO(Ivan) : Scrollbar
+        ClipRRect(
+          borderRadius: BorderRadius.circular(5),
+          child: Stack(
+            children: [
+              Container(
+                width: 200,
+                height: 7,
+                decoration: BoxDecoration(
+                  color: const Color(
+                    0xFFC4C4C4,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              AnimatedPositioned(
+                duration: const Duration(milliseconds: 100),
+                left: _lineLeft,
+                child: Container(
+                  width: 50,
+                  height: 7,
+                  decoration: BoxDecoration(
+                    color: const Color(
+                      0xFF9F9F9F,
+                    ).withOpacity(.68),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
